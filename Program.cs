61a879a0db98e39fe36b405f3617c3af5cc1ca68
robertusakovTARpv24 <0e.m.v.o.r.t.e.x0@gmail.com>
2025-01-31 16:54:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace NameControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //programm küsib nime
+            //kui siséstasid nime, siis konsool vastab
+            //TERE, sinu nimi
+            //peab kasutama if ja else
+            //kui nime ei sisetata, siis tuleb vastuseks
+            //ERROR ja konsool teeb Beep kolm korda
+
+
+            Console.WriteLine("Sisetaja enda nimi ja vajuta´enter");
+            string name = Console.ReadLine();
+
+            if (name !== "")
+            {
+                Console.WriteLine("TEre " + name);
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR");
+                Console.Beep()
+                Console.Beep()
+                Console.Beep()
+            }
+        }
+    }
+
+}
